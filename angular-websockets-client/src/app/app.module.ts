48feea7adc657b27d09e636f84c3a7e86b8f22c4
bart-van-pelt <bart.vanpelt@kadaster.nl>
2021-07-15 11:49:54 +0200

    client

--- angular-websockets-client/src/app/app.module.ts
+++ angular-websockets-client/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {Route, RouterModule} from '@angular/router';
+
+const routes: Route[] = [
+  {path: '', redirectTo: 'posts', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
